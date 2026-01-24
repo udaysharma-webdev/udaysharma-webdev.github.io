91cf63fe573b335e13d9c0e320f2598f214a1502
udaysharma-webdev <udaysharma561@gmail.com>
2026-01-24 16:35:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-setInterval(() => {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
-  slides[index].classList.add("active");
-}, 3500);
 
