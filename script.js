e5837cb308a7359d5a005d432a48b9c21f2d0c92
udaysharma-webdev <udaysharma561@gmail.com>
2026-01-24 16:14:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const tracks = document.querySelectorAll(".project-track, .skills-track");
 
-tracks.forEach(track => {
-  track.addEventListener("mouseenter", () => {
-    track.style.animationPlayState = "paused";
-  });
-  track.addEventListener("mouseleave", () => {
-    track.style.animationPlayState = "running";
-  });
-});
